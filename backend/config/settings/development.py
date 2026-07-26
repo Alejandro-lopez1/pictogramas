8b9cc3ffd0242cdae250f369dfadd97c8f7c7c0b
Alejandro Lopez <alejandro.lopez14591@gmail.com>
2026-07-25 21:53:43 -0300

    feat(settings): configure env-based settings with postgresql

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -2,6 +2,4 @@ from .base import *  # noqa: F401, F403
 
 DEBUG = True
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="localhost,127.0.0.1", cast=Csv())  # noqa: F405
-
-CORS_ALLOW_ALL_ORIGINS = config("CORS_ALLOW_ALL_ORIGINS", default=True, cast=bool)  # noqa: F405
+CORS_ALLOW_ALL_ORIGINS = True
--- backend/config/settings/production.py
+++ backend/config/settings/production.py
@@ -2,15 +2,7 @@ from .base import *  # noqa: F401, F403
 
 DEBUG = False
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="", cast=Csv())  # noqa: F405
-
 CORS_ALLOW_ALL_ORIGINS = False
-
-CORS_ALLOWED_ORIGINS = config(  # noqa: F405
-    "CORS_ALLOWED_ORIGINS",
-    default="",
-    cast=Csv(),
-)
 
 SECURE_BROWSER_XSS_FILTER = True
 SECURE_CONTENT_TYPE_NOSNIFF = True
